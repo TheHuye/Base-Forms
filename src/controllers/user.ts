c13634dfb100af25ff564d0e2736c9412c276f9a
Derrick-Nuby <iradukundaderrick7@gmail.com>
2024-04-26 22:54:50 +0200

    added code functionality for controllers

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,11 +1,19 @@
 import { Response, Request, NextFunction  } from "express"
 import { IUser } from "../types/user"
-// import { User } from "../models/user"
+import User from "../models/user.js"
+import fs from 'fs';
+import path from 'path';
 
 const getAllUsers = async (req: Request, res: Response): Promise<any> => {
     try {
-        console.log("getAllUsers Logged");
-            
+
+        const users: IUser[] = await User.find();
+
+        if (users.length === 0) {
+            return res.status(404).json({ error: "There are currently no users found in the database! Thank you for checking :)"})
+        }
+
+        return res.status(200).json({ message: "These are all the users you have now!", users})
     } catch (error) {
     throw error
     }
@@ -13,8 +21,12 @@ const getAllUsers = async (req: Request, res: Response): Promise<any> => {
 
 const createUser = async (req: Request, res: Response): Promise<any> => {
     try {
-        console.log("createUser Logged");
-            
+        
+        const user: IUser = new User(req.body)
+        const newUser: IUser = await user.save()
+
+        res.status(201).json({ message: "User created successfully", User: newUser})
+
     } catch (error) {
     throw error
     }
@@ -22,8 +34,15 @@ const createUser = async (req: Request, res: Response): Promise<any> => {
 
 const getSingleUser = async (req: Request, res: Response): Promise<any> => {
     try {
-        console.log("getSingleUser Logged");
-            
+
+        const userId = req.params.id;
+        const singleUser: IUser | null = await User.findOne({ _id: userId });
+
+        if (!singleUser) {
+            res.status(404).json({ error: "That user doesn't exist on our database"})
+        }
+
+        res.status(200).json({ message: "This is the user you requested", user: singleUser})
     } catch (error) {
     throw error
     }
@@ -31,7 +50,20 @@ const getSingleUser = async (req: Request, res: Response): Promise<any> => {
 
 const updateUser = async (req: Request, res: Response): Promise<any> => {
     try {
-        console.log("updateUser Logged");
+        const userId = req.params.id;
+        const updateFields = req.body;
+
+        const user: IUser | null = await User.findById(userId);
+
+        if (!user) {
+            res.status(404).json({ error: "That use you are trying to update doesn't exist" });
+
+            return;
+        }
+
+        const updatedUser: IUser | null = await User.findOneAndUpdate( { _id: userId}, updateFields, { new: true });
+
+        res.status(200).json({ message: "User updated successfully", user: updatedUser})
             
     } catch (error) {
     throw error
@@ -40,10 +72,43 @@ const updateUser = async (req: Request, res: Response): Promise<any> => {
 
 const exportUser = async (req: Request, res: Response): Promise<any> => {
     try {
-        console.log("exportUser Logged");
+        const userId = req.params.id;
+        const user: IUser | null = await User.findById(userId);
+
+        if (!user) {
+            return res.status(404).json({ error: "That user you are trying to update doesn't exist" });
+        }
+
+        // const exportName = user.idNumber;
+
+        let userDetailsString = '';
+        Object.entries(user.toJSON()).forEach(([key, value]) => {
+            userDetailsString += `${key}: ${value}\n`;
+        });
+
+        const directory = path.join(process.cwd(), 'exports');
+
+        if (!fs.existsSync(directory)) {
+            fs.mkdirSync(directory);
+        }
+
+        const filePath = path.join(directory, `${userId}.txt`);
+
+        const fileName = `${userId}.txt`;
+        fs.writeFile(filePath, userDetailsString, (err) => {
+            if (err) {
+                throw err;
+            }
+            console.log(`User details saved to ${fileName}`);
+        });
+
+        const message = `User details successfully exported. File saved as ${fileName}`;
+
+        res.status(200).json({ message });
             
     } catch (error) {
-    throw error
+        console.error(error);
+        res.status(500).json({ error: "An error occurred while exporting user details" });
     }
 }
 
@@ -58,11 +123,48 @@ const archieveUser = async (req: Request, res: Response): Promise<any> => {
 
 const deleteUser = async (req: Request, res: Response): Promise<any> => {
     try {
-        console.log("deleteUser Logged");
+        const userId = req.params.id;
+        const user: IUser | null = await User.findById(userId);
+
+        if (!user) {
+            res.status(404).json({ error: "That use you are trying to delete doesn't exist" });
+
+            return;
+        }
+
+        const deletedUser = await User.findOneAndDelete({ _id: userId });
+
+        res.status(200).json({ message: "User deleted successfully", user: deletedUser})
             
     } catch (error) {
     throw error
     }
 }
 
-export { getAllUsers, createUser, getSingleUser, updateUser, exportUser, archieveUser, deleteUser, }
+const downloadUserDetails = async (req: Request, res: Response): Promise<any> => {
+    try {
+        const userId = req.params.id;
+        const user = await User.findById(userId);
+
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        // Generate the user details text content
+        const userDetailsString = Object.entries(user.toJSON())
+            .map(([key, value]) => `${key}: ${value}`)
+            .join('\n');
+
+        // Set appropriate headers for downloading the file
+        res.setHeader('Content-Disposition', `attachment; filename=${userId}.txt`);
+        res.setHeader('Content-Type', 'text/plain');
+
+        // Send the user details text content as a response
+        res.send(userDetailsString);
+            
+    } catch (error) {
+    throw error
+    }
+}
+
+export { getAllUsers, createUser, getSingleUser, updateUser, exportUser, archieveUser, deleteUser, downloadUserDetails }
