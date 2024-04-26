0bd98ac7bda7b0d6863b83dfcbb1a7793bd27b41
Derrick-Nuby <iradukundaderrick7@gmail.com>
2024-04-26 12:02:07 +0200

    tsc generate js dist

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,18 @@
-"use strict";
-console.log("Project setup");
+import express from "express";
+import mongoose from "mongoose";
+import userRoutes from "./routes/user.js";
+import dotenv from 'dotenv';
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 4000;
+app.use(express.json());
+app.use('/api/v1/user', userRoutes);
+const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/Base-Forms';
+mongoose
+    .connect(uri)
+    .then(() => app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`)))
+    .catch(error => {
+    throw error;
+});
+export default app;
 //# sourceMappingURL=app.js.map
