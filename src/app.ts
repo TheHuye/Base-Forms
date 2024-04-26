c7c3736db98215ddd01b98e019c9d4a04badf83e
Derrick-Nuby <iradukundaderrick7@gmail.com>
2024-04-26 12:01:12 +0200

    project initializations routes definitions

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,34 @@
-console.log("Project setup");
+import express, { Express } from "express"
+import mongoose from "mongoose"
+import userRoutes from "./routes/user.js"
+
+
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app: Express = express()
+
+const PORT: string | number = process.env.PORT || 4000
+
+
+app.use(express.json());
+app.use('/api/v1/user', userRoutes)
+
+const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/Base-Forms';
+
+
+mongoose
+    .connect(uri)
+    .then(() =>
+    app.listen(PORT, () =>
+        console.log(`Server running on http://localhost:${PORT}`)
+    )
+)
+.catch(error => {
+    throw error
+})
+
+
+
+export default app
