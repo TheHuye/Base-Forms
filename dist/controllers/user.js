c318d9461044c9022ddcf22379a1b79abbb4e492
Derrick-Nuby <iradukundaderrick7@gmail.com>
2024-04-26 12:02:07 +0200

    tsc generate js dist

---
+++ dist/controllers/user.js
@@ -0,0 +1,59 @@
+// import { User } from "../models/user"
+const getAllUsers = async (req, res) => {
+    try {
+        console.log("getAllUsers Logged");
+    }
+    catch (error) {
+        throw error;
+    }
+};
+const createUser = async (req, res) => {
+    try {
+        console.log("createUser Logged");
+    }
+    catch (error) {
+        throw error;
+    }
+};
+const getSingleUser = async (req, res) => {
+    try {
+        console.log("getSingleUser Logged");
+    }
+    catch (error) {
+        throw error;
+    }
+};
+const updateUser = async (req, res) => {
+    try {
+        console.log("updateUser Logged");
+    }
+    catch (error) {
+        throw error;
+    }
+};
+const exportUser = async (req, res) => {
+    try {
+        console.log("exportUser Logged");
+    }
+    catch (error) {
+        throw error;
+    }
+};
+const archieveUser = async (req, res) => {
+    try {
+        console.log("archieveUser Logged");
+    }
+    catch (error) {
+        throw error;
+    }
+};
+const deleteUser = async (req, res) => {
+    try {
+        console.log("deleteUser Logged");
+    }
+    catch (error) {
+        throw error;
+    }
+};
+export { getAllUsers, createUser, getSingleUser, updateUser, exportUser, archieveUser, deleteUser, };
+//# sourceMappingURL=user.js.map
---
+++ dist/enums/enums.js
@@ -0,0 +1,21 @@
+// enums.ts
+export var Gender;
+(function (Gender) {
+    Gender["Male"] = "male";
+    Gender["Female"] = "female";
+    Gender["Other"] = "other";
+})(Gender || (Gender = {}));
+export var MaritalStatus;
+(function (MaritalStatus) {
+    MaritalStatus["Single"] = "single";
+    MaritalStatus["Married"] = "married";
+    MaritalStatus["Divorced"] = "divorced";
+    MaritalStatus["Widowed"] = "widowed";
+})(MaritalStatus || (MaritalStatus = {}));
+export var schoolCategory;
+(function (schoolCategory) {
+    schoolCategory["TTC"] = "ttc";
+    schoolCategory["TVET"] = "tvet";
+    schoolCategory["GE"] = "General Education";
+})(schoolCategory || (schoolCategory = {}));
+//# sourceMappingURL=enums.js.map
