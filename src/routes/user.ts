35b7ce3480eed3615d6b75a3f421ee61b2894f34
Derrick-Nuby <iradukundaderrick7@gmail.com>
2024-04-27 11:39:10 +0200

    user search

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,16 +1,18 @@
-import { getAllUsers, createUser, getSingleUser, updateUser, exportUser, archieveUser, deleteUser, downloadUserDetails, } from "../controllers/user.js"
+import { getAllUsers, createUser, getSingleUser, updateUser, exportUser, archieveUser, deleteUser, downloadUserDetails, searchUsers} from "../controllers/user.js"
 import { Router } from "express"
+import { validateUserRegister } from "../middleware/userValidation.js"
 
 const router: Router = Router()
 
 router.get("/", getAllUsers)
-router.post("/", createUser)
+router.post("/", validateUserRegister, createUser)
 router.get("/:id", getSingleUser)
 router.put("/:id", updateUser)
 router.get("/export/:id", exportUser)
 router.get("/archieve/:id", archieveUser)
 router.delete("/:id", deleteUser)
 router.get("/download/:id", downloadUserDetails)
+router.get("/search", searchUsers)
 
 export default router
 
