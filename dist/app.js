e847e0d1738b6a9569e990654c460a6c3d260e75
Derrick-Nuby <iradukundaderrick7@gmail.com>
2024-04-30 13:08:35 +0200

    cors

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
 import userRoutes from "./routes/user.js";
+import cors from "cors";
 import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4000;
+app.use(cors({
+    origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use('/api/v1/user', userRoutes);
 const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/Base-Forms';
