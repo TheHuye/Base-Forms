5cbc002b5fa1d2b701a8dca5dfd4e10c34bbbf50
Derrick-Nuby <iradukundaderrick7@gmail.com>
2024-06-03 23:32:37 +0200

    romoved dist folder

--- dist/middleware/multer.js
+++
@@ -1,7 +0,0 @@
-import multer from 'multer';
-// Set up Multer storage
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
-// Middleware to handle file uploads
-export const uploadFile = upload.single('file'); // Assuming your file input field is named 'file'
-//# sourceMappingURL=multer.js.map
